88caf81b4584cf04fa1d9c186b7f2640f3bcacb0
Dumebi <chiwuzohdumebi@gmail.com>
2020-12-31 13:22:14 +0100

    added node engine

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb+srv://TheDumebi:" + process.env.DB_PASSWORD +"@cluster0.zrvvc.mongodb.net/100daysDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+mongoose.connect("mongodb+srv://TheDumebi:" + process.env.DB_PASSWORD +"@cluster0.zrvvc.mongodb.net/100daysDB?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
 // mongoose.connect("mongodb://localhost:27017/100daysDB", {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false});
 
 const articleSchema = mongoose.Schema ({
