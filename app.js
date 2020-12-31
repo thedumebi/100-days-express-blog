cbc56269fdce437e20f5b6689db10d2026d64860
Dumebi <chiwuzohdumebi@gmail.com>
2020-12-31 13:26:45 +0100

    edit static public

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static(__dirname +"/public"));
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
